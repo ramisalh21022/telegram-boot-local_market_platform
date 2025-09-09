cb6c10dfe42627ea9bc7fb1f0c2781e3608aeaec
ramisalh21022 <134399309+ramisalh21022@users.noreply.github.com>
2025-09-08 23:40:54 -0700

    Update db.js

--- db.js
+++ db.js
@@ -2,10 +2,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.SUPABASE_URL,       // اسم المستخدم على Supabase
-  host: process.env.SUPABASE_ANON_KEY,       // مثل db.xyz.supabase.co
-  
-  
+  user: process.env.DB_USER,       // اسم المستخدم على Supabase
+  host: process.env.DB_HOST,       // مثل db.xyz.supabase.co
+  database: process.env.DB_NAME,   // اسم قاعدة البيانات
+  password: process.env.DB_PASSWORD,
+  port: 5432,
   ssl: { rejectUnauthorized: false } // مهم للاتصال عبر Supabase
 });
 
